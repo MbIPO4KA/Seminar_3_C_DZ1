758fcb90e1054ebaf6d0707a1c6d203e66f3712a
MbIPO4KA <63498319+MbIPO4KA@users.noreply.github.com>
2023-02-24 12:30:18 +0500

    Задача на полиандром выполнена

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число
+ и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+Console.Write("Введите пятизначное число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+
+int result = (namber / 10000) % 10 ;
+int result2 = namber  % 10 ;
+int result3 = (namber / 1000) % 10 ;
+int result4 = (namber / 10) % 10 ;
+
+if (namber >= 10000 && namber < 100000  )   
+{
+    Console.WriteLine($"первая цифра этого числа  {result}  ");      
+    Console.WriteLine($"последняя цифра этого числа  {result2}  ");
+    Console.WriteLine($"вторая цифра этого числа  {result3}  ");      
+    Console.WriteLine($"четвертая цифра этого числа  {result4}  ");
+    if (result==result2 && result3==result4)
+    {
+        Console.WriteLine("Число является  палиндромом. ");
+    }
+    else
+    {
+        Console.WriteLine("Число НЕ является  палиндромом. ");
+    }
+}
+else
+{
+     Console.Write($"Нет, число {namber} не пятизначное."); 
+        
+}
